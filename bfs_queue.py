4bc2653756b3dc39cddb3fb4852dfd1ed37ee4fb
nadziroar <nadzirahzaid144@gmail.com>
2022-02-16 14:48:56 +0800

    Update bfs_queue.py

--- bfs_queue.py
+++ bfs_queue.py
@@ -1,25 +1,43 @@
-#BFS algorithm in Python
+#Python3 Program to print BFS traversal from a given source vertex. BFS (int s ) traverses vertices reachable from s.
+from collections import defaultdict
+import queue
 
-import collections
-
-#BFS algorithm
-def bfs(graph , root):
+#This class represents a directed graph using adjancency list representation
+class Graph:
     
-    visited , queue = set() , collections.deque([root])
-    visited.add(root)
-    
-    while queue:
-        #Dequeue a vertex from queue
-        vertex = queue.popleft()
-        print(str(vertex) + " ", end = " ")
+    #Constructor:
+    def __init__(self):
+        #Default dictionary to store graph
+        self.graph = defaultdict(list)
         
-        #If not visited , mark it as visited and enqueue it
-        for neighbour in graph[vertex]:
-            if neighbour not in visited:
-                visited.add(neighbour)
-                queue.append(neighbour)
-                
-if __name__ == '__main__' : 
-    graph = {0: [1,2] , 1: [2] , 2: [3] , 3: [1,2]}
-    print("Following is Breadth First Traversal : ")
-    bfs(graph, 0 )
+    #Function to add edge to graph
+    def addEdge(self , u , v):
+        self.graph[u].append(v)
+        
+    #Function to print a BFS of graph
+    def BFS(self , s):
+        #Mark all the vertices as not visited 
+        visited = [False] * (max(self.graph) + 1)
+        
+        #Create a queue for BFS
+        queue = []
+        
+        #Mark the source node as visited and enqueue it
+        queue.append(s)
+        visited[s] = True
+        
+        while queue:
+            #Dequeue a vertex from queue and print it
+            s = queue.pop(0)
+            print(s , end = " ")
+            
+            #Get all adjacent vertices of the dequeued vertexs. If a adjacent has not been visited, then mark it visited and enqueue it.
+            for i in self.graph[s]:
+                if visited[i] == False:
+                    queue.append(i)
+                    visited[i] = True
+                    
+#Driver Code
+#Create a graph given in the above diagram
+g = Graph()
+g.addEdge(0,1)
