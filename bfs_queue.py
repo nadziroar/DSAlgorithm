dcd67b58bce05cb0d568598f5ea7775dc18e7632
nadziroar <nadzirahzaid144@gmail.com>
2022-03-10 07:08:54 +0800

    Update bfs_queue.py

--- bfs_queue.py
+++ bfs_queue.py
@@ -45,3 +45,12 @@ class Graph:
 #Create a graph given in the above diagram
 g = Graph()
 g.addEdge(0,1)
+g.addEdge(0,2)
+g.addEdge(1,2)
+g.addEdge(2,0)
+g.addEdge(2,3)
+g.addEdge(3,3)
+
+print("Following is Breadth First Traversal" "(starting from vertex 2)")
+
+g.BFS(2)
